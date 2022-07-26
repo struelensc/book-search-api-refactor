05e5e6f92e75d1c3eadddfb1451f40a6a8ef8826
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-26 12:27:37 -0700

    wired login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,24 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
   },
-  Mutation: {},
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
