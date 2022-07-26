5d2950a11e7357d624a90c5537f257322512797e
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-26 16:19:09 -0700

    fixed bug for loginUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,7 @@ const resolvers = {
   },
 
   Mutation: {
-    login: async (parent, { email, password }) => {
+    loginUser: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
