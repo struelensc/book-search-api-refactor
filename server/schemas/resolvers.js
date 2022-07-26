8dd9f79aa3bfa63e6596e83a3b6bd0882086d1f6
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-26 12:25:03 -0700

    wired typedefs and me query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {},
+};
+
+module.exports = resolvers;
