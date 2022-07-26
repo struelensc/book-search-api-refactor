69a90dd14cc09b6cffe9bd257653efe01832b691
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-26 14:50:20 -0700

    fixed bracket bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,7 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
   },
+
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -27,43 +28,46 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-  },
-  addUser: async (parent, { name, email, password }) => {
-    const user = await User.create({ name, email, password });
-    const token = signToken(user);
 
-    return { token, user };
-  },
-  saveBook: async (
-    parent,
-    { authors, description, title, bookId, image, link },
-    context
-  ) => {
-    if (context.user) {
-      return User.findOneAndUpdate(
-        { _id: context.user._id },
-        {
-          $addToSet: {
-            savedBooks: { authors, description, title, bookId, image, link },
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    saveBook: async (
+      parent,
+      { authors, description, title, bookId, image, link },
+      context
+    ) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              savedBooks: { authors, description, title, bookId, image, link },
+            },
           },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    }
-    throw new AuthenticationError("You need to be logged in!");
-  },
-  removeBook: async (parent, { bookId }, context) => {
-    if (context.user) {
-      return User.findOneAndUpdate(
-        { _id: context.user._id },
-        { $pull: { savedBooks: bookId } },
-        { new: true }
-      );
-    }
-    throw new AuthenticationError("You need to be logged in!");
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: bookId } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
 };
 
