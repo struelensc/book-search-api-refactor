0de18945dbf5a8f70a7ce22a5d3eadde1658877f
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-26 23:21:41 -0700

    debugging deploy

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,7 +14,7 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
